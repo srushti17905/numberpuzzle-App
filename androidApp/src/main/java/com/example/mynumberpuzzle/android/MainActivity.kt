b9718067fe140f1dacebe1427ffe543769635a1a
srushtirathod <srushtirathod1795@gmail.com>
2024-08-05 18:14:48 +0530

    sr

--- androidApp/src/main/java/com/example/mynumberpuzzle/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/mynumberpuzzle/android/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.mynumberpuzzle.android
 
-import android.os.Bundle
+import  android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,34 +8,30 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsEndWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.pm.ShortcutInfoCompat.Surface
-import com.example.mynumberpuzzle.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,21 +42,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// github
-
-// git pull project
-
 @Composable
 @Preview
 fun numberpuzzle() {
-
     Image(
         painter = painterResource(id = R.drawable.np1),
         contentDescription = null,
         contentScale = ContentScale.FillBounds
     )
-    Column {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    )
+    {
         Row(
             modifier = Modifier
                 .fillMaxSize()
@@ -74,7 +68,8 @@ fun numberpuzzle() {
                 .fillMaxSize()
                 .weight(0.1f)
                 .background(color = rainbow.lightcream)
-        ) {
+        )
+        {
         }
         Row(
             modifier = Modifier
@@ -83,43 +78,38 @@ fun numberpuzzle() {
         )
         {
         }
-        Row(modifier = Modifier
-            .fillMaxSize()
-            .weight(0.02f)
-            .background(color = rainbow.brown))
-        {
-
-        }
         Row(
             modifier = Modifier
                 .fillMaxSize()
-                .weight(1f)
+                .weight(0.03f)
+                .background(color = rainbow.brown)
         )
         {
-            square(rowScope = this, color = rainbow.cream, text = "1")
-            square(rowScope = this, color = rainbow.cream, text = "2")
-            square(rowScope = this, color = rainbow.cream, text = "3")
         }
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1f)
-        ) {
-            square(rowScope = this, color = rainbow.cream, text = "4")
-            square(rowScope = this, color = rainbow.cream, text = "5")
-            square(rowScope = this, color = rainbow.cream, text = "6")
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1f)
-        ) {
-            square(rowScope = this, color = rainbow.cream, text = "7")
-            square(rowScope = this, color = rainbow.cream, text = "8")
-            square(rowScope = this, color = rainbow.cream, text = "")
-        }
-        Row (modifier = Modifier.fillMaxSize().weight(0.02f).background(color = rainbow.brown)){
 
+        var tt = 0
+
+        for (ii in 1..3) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+            {
+                for (i in 1..3) {
+                    square(rowScope = this, color = rainbow.cream, tt)
+                    tt++
+                }
+
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.03f)
+                .background(color = rainbow.brown)
+        ) {
         }
         Box(
             modifier = Modifier
@@ -129,7 +119,6 @@ fun numberpuzzle() {
         {
             button(text = "Restart")
         }
-
         Row(
             modifier = Modifier
                 .fillMaxSize()
@@ -137,22 +126,22 @@ fun numberpuzzle() {
                 .background(color = rainbow.lightcream)
         )
         {
-
         }
-
         Row(
             modifier = Modifier
                 .fillMaxSize()
                 .weight(0.4f)
                 .background(color = rainbow.brown)
         ) {
-
         }
     }
 }
 
+var list = mutableStateListOf<String>("1","2","3","4","5","6","7","8","")
+var number = mutableStateOf("")
+
 @Composable
-fun square(rowScope: RowScope, color: Color, text: String) {
+fun square(rowScope: RowScope, color: Color , i : Int ) {
     rowScope.apply {
         Surface(
             modifier = Modifier
@@ -161,14 +150,81 @@ fun square(rowScope: RowScope, color: Color, text: String) {
                 .border(BorderStroke(1.1.dp, color = rainbow.brown)), color = color
         )
         {
-            Surface(
+            Box(contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1f)
-                    .padding(9.dp), color = rainbow.light
+                    .padding(9.dp)
+                    .background(color = rainbow.light)
+                    .clickable {
+
+                            if (list[i] == "8" && list[8] == "") {
+                                list[7] = ""
+                                list[8] = "8"
+                            } else if (list[i] == "8" && list[7] == "") {
+                                list[8] = ""
+                                list[7] = "8"
+                            } else if (list[i] == "7" && list[7] == "") {
+                                list[6] = ""
+                                list[7] = "7"
+                            } else if (list[i] == "7" && list[6] == "") {
+                                list[7] = ""
+                                list[6] = "7"
+                            } else if (list[i] == "6" && list[8] == "") {
+                                list[5] = ""
+                                list[8] = "6"
+                            } else if (list[i] == "6" && list[5] == "") {
+                                list[8] = ""
+                                list[5] = "6"
+                            } else if (list[i] == "5" && list[5] == "") {
+                                list[4] = ""
+                                list[5] = "5"
+                            } else if (list[i] == "5" && list[4] == "") {
+                                list[5] = ""
+                                list[4] = "5"
+                            } else if(list[i] == "5" && list[1] == "")
+                            {
+                                list[4] = ""
+                                list[1] = "5"
+                            }
+                            else if (list[i] == "4" && list[4] == "") {
+                                list[3] = ""
+                                list[4] = "4"
+                            } else if (list[i] == "4" && list[3] == "") {
+                                list[4] = ""
+                                list[3] = "4"
+                            } else if (list[i] == "3" && list[5] == "") {
+                                list[2] = ""
+                                list[5] = "3"
+                            } else if (list[i] == "3" && list[2] == "") {
+                                list[5] = ""
+                                list[2] = "3"
+                            } else if (list[i] == "2" && list[2] == "") {
+                                list[1] = ""
+                                list[2] = "2"
+                            } else if (list[i] == "2" && list[1] == "") {
+                                list[2] = ""
+                                list[1] = "2"
+                            } else if (list[i] == "1" && list[1] == "") {
+                                list[0] = ""
+                                list[1] = "1"
+                            } else if (list[i] == "1" && list[0] == "") {
+                                list[1] = ""
+                                list[0] = "1"
+                            } else if (list[i] == "1" && list[2] == "") {
+                                list[1] = ""
+                                list[2] = "1"
+                            } else if (list[i] == "1" && list[1] == "") {
+                                list[2] = ""
+                                list[1] = "1"
+                            } else if (list[i] == "1" && list[5] == "") {
+                                list[2] = ""
+                                list[5] = "1"
+                            }
+                    }
             ) {
                 Text(
-                    text = text,
+                    text = list[i],
                     fontSize = 80.sp,
                     color = rainbow.brown,
                     textAlign = TextAlign.Center
@@ -178,12 +234,11 @@ fun square(rowScope: RowScope, color: Color, text: String) {
     }
 }
 
-var number = mutableStateOf("")
-
 @Composable
 fun button(text: String) {
     Button(
-        onClick = { Log.d("==>", "Button : ") },
+        onClick = { Log.d("==>", "Button : ")
+                  },
         enabled = true,
         colors = ButtonDefaults.buttonColors(rainbow.brown),
         shape = RoundedCornerShape(50),
